a00b1850ac5ffff5c4aff3d35e4736ac1b037b48
Matthew Morton <mm9rq@virginia.edu>
2023-03-21 15:35:55 -0400

    syncing up frontend

--- FrontEnd.js
+++ FrontEnd.js
@@ -1,23 +1,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-  });
-  
-  
-  const functionApi = 'https://azcloudfunctionapp.azurewebsites.net/api/HttpTrigger1?code=OrDF2X98oYhqiCe9CkzdbwmQ1cXVlWIKVpQog6WhjksKAzFuDhuddA==&clientId=blobs_extension';
-  
-  const getVisitCount = () => {
-    let count = 7;
+});
+
+
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://azcloudfunctionapp.azurewebsites.net/api/HttpTrigger1?code=OrDF2X98oYhqiCe9CkzdbwmQ1cXVlWIKVpQog6WhjksKAzFuDhuddA==&clientId=blobs_extension'; 
+
+const getVisitCount = () => {
+    let count = 30;
     fetch(functionApi)
-      .then(response => {
+    .then(response => {
         return response.json()
-      })
-      .then(response => {
-        count = response;
-        console.log("Hello 👋, you are visitor number - " + count);
+    })
+    .then(response => {
+        console.log("Website called function API.");
+        count = response.count;
         document.getElementById('counter').innerText = count;
-      }).catch(function (error) {
+    }).catch(function(error) {
         console.log(error);
       });
     return count;
-  }
+}
   
