a726c84a87b39c62c8e9b6eed9edd55f43440643
Matthew Morton <mm9rq@virginia.edu>
2023-04-04 17:14:28 -0400

    made sure counter worked

--- main.js/main.js
+++ main.js/main.js
@@ -12,7 +12,7 @@ const getVisitCount = () => {
       .then(data => {
         console.log("Website called function API.");
         count = data.count;
-        document.getElementById('visitors').textContent = count;
+        document.getElementById('counter').textContent = count;
       })
       .catch(error => {
         console.log(error);
