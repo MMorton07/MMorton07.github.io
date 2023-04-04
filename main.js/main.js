69318aece37184a5e956712d359f01b8e0e05e1b
Matthew Morton <mm9rq@virginia.edu>
2023-04-04 17:16:06 -0400

    undid trial changes

--- main.js/main.js
+++ main.js/main.js
@@ -1,18 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
+    visitors();
 });
 
 
 const functionApi = 'https://azurefunctionappmm.azurewebsites.net/api/HttpTrigger1?code=1KSIgslz5jq-EaNZeOWYgsKXfNKbBjVj3wRY0IyvZ6buAzFu9PEEcA=='; 
 
-const getVisitCount = () => {
+const visitors = () => {
     let count = 30;
     fetch(functionApi)
       .then(response => response.json())
       .then(data => {
         console.log("Website called function API.");
         count = data.count;
-        document.getElementById('counter').textContent = count;
+        document.getElementById('visitors').textContent = count;
       })
       .catch(error => {
         console.log(error);
