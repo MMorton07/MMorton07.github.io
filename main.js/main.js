81984da63b808f9eafeb6ce7e3b839dfaa2d432f
Matthew Morton <mm9rq@virginia.edu>
2023-04-04 14:28:52 -0400

    adding counter to github website

--- main.js/main.js
+++ main.js/main.js
@@ -3,7 +3,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
-const localApi = 'http://localhost:7071/api/GetResumeCounter';
 const functionApi = 'https://azurefunctionappmm.azurewebsites.net/api/HttpTrigger1?code=1KSIgslz5jq-EaNZeOWYgsKXfNKbBjVj3wRY0IyvZ6buAzFu9PEEcA=='; 
 
 const visitors = () => {
@@ -14,7 +13,7 @@ const visitors = () => {
     })
     .then(response => {
         console.log("Website called function API.");
-        count = response <br />document.getElementById('counter').innerText = count;
+        count = response <br />document.getElementById('visitors').innerText = count;
 
     }).catch(function(error) {
         console.log(error);
