cbd873774bdcbbcffc3bdc2d7e67e6c5ec0b1359
Matthew Morton <mm9rq@virginia.edu>
2023-04-04 17:11:31 -0400

    getvisitcount command

---
+++ main.js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
+
+
+const functionApi = 'https://azurefunctionappmm.azurewebsites.net/api/HttpTrigger1?code=1KSIgslz5jq-EaNZeOWYgsKXfNKbBjVj3wRY0IyvZ6buAzFu9PEEcA=='; 
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi)
+      .then(response => response.json())
+      .then(data => {
+        console.log("Website called function API.");
+        count = data.count;
+        document.getElementById('counter').textContent = count;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    return count;
+  };
+  
